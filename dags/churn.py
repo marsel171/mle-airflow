de5e2eafc0946169d2f8126b80ad9faf134ad04e
marsel171 <mle-user@epdssk1nj101vqqm13hv.auto.internal>
2024-09-23 17:13:57 +0000

    upd dag

--- dags/churn.py
+++ dags/churn.py
@@ -4,13 +4,49 @@ from airflow.decorators import dag, task
 @dag(
     schedule='@once',
     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
     tags=["ETL"]
 )
+
 def prepare_churn_dataset():
     import pandas as pd
     import numpy as np
     from airflow.providers.postgres.hooks.postgres import PostgresHook
-   
+
+    @task()
+    def create_table() -> None:
+        from sqlalchemy import MetaData, Table, Column, String, Integer, DateTime, UniqueConstraint, inspect, Float
+
+        db_conn = PostgresHook("destination_db").get_sqlalchemy_engine().connect()
+        metadata = MetaData()
+        table = Table('users_churn', metadata, 
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('customer_id', String),
+            Column('begin_date', DateTime),
+            Column('end_date', DateTime),
+            Column('type', String),
+            Column('paperless_billing', String),
+            Column('payment_method', String),
+            Column('monthly_charges', Float),
+            Column('total_charges', Float),
+            Column('internet_service', String),
+            Column('online_security', String),
+            Column('online_backup', String),
+            Column('device_protection', String),
+            Column('tech_support', String),
+            Column('streaming_tv', String),
+            Column('streaming_movies', String),
+            Column('gender', String),
+            Column('senior_citizen', Integer),
+            Column('partner', String),
+            Column('dependents', String),
+            Column('multiple_lines', String),
+            Column('target', Integer),
+            UniqueConstraint('customer_id', name='unique_customer_id_constraint')
+            )
+        if not inspect(db_conn).has_table(table.name): 
+            metadata.create_all(db_conn) 
+
     @task()
     def extract(**kwargs):
         """
@@ -56,6 +92,7 @@ def prepare_churn_dataset():
             rows=data.values.tolist()
     )
 
+    create_table() 
     data = extract()
     transformed_data = transform(data)
     load(transformed_data)
